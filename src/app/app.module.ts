6fc61746ba5aea415d3a44d13f7872abbf25c8d2
crushcardtopo01 <silentlv5@gmail.com>
2021-02-17 10:09:24 -0600

    Agrege el componente Modal y se inicializo el componente Informacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './modal/modal.component';
+import { RecomendacionesComponent } from './recomendaciones/recomendaciones.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    ModalComponent,
+    RecomendacionesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
