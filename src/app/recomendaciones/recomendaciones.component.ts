8891787c5e905e2667c4c19bf5a9c41ffcad4370
crushcardtopo01 <silentlv5@gmail.com>
2021-02-17 10:48:27 -0600

    se agregan videos al modal de Recomendaciones

--- src/app/recomendaciones/recomendaciones.component.ts
+++ src/app/recomendaciones/recomendaciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-recomendaciones',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecomendacionesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modal:NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  openModalRecomendaciones(contenido){
+    this.modal.open(contenido,{size:'xl',scrollable:true});
+
+
   }
 
 }
